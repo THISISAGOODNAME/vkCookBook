f15f543fc1067c2e6c4ca672899e3ef478a994e5
yanjunyang.yyj <yanjunyang.yyj@alibaba-inc.com>
2017-06-13 15:24:53 +0800

    VulkanFunctions

---
+++ book/common/VulkanFunctions.cpp
@@ -0,0 +1,18 @@
+//
+// Created by yangyanjun on 2017/6/13.
+//
+
+#include "VulkanFunctions.h"
+
+namespace VKCookbook {
+
+#define EXPORTED_VULKAN_FUNCTION( name ) PFN_##name name;
+#define GLOBAL_LEVEL_VULKAN_FUNCTION( name ) PFN_##name name;
+#define INSTANCE_LEVEL_VULKAN_FUNCTION( name ) PFN_##name name;
+#define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension ) PFN_##name name;
+#define DEVICE_LEVEL_VULKAN_FUNCTION( name ) PFN_##name name;
+#define DEVICE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( name, extension ) PFN_##name name;
+
+#include "ListOfVulkanFunctions.inl"
+
+};
