fc523b2582ae8955ae3d67225233b7787a16b92c
yanjun <yangyj19931231@gmail.com>
2017-06-17 13:20:49 +0800

    CH01 Instance and Devices

---
+++ book/CH01/CH01_Instance_and_Devices.h
@@ -0,0 +1,27 @@
+//
+// Created by aicdg on 2017/6/17.
+//
+
+#ifndef VKCOOKBOOK_CH01_INSTANCE_AND_DEVICES_H
+#define VKCOOKBOOK_CH01_INSTANCE_AND_DEVICES_H
+
+#include "S03_Connecting_with_a_Vulkan_Loader_library.h"
+#include "S05_Loading_function_exported_from_a_Vulkan_Loader_library.h"
+#include "S06_Loading_global_level_functions.h"
+#include "S07_Checking_available_Instance_extensions.h"
+#include "S08_Creating_a_Vulkan_Instance.h"
+#include "S09_Loading_instance_level_functions.h"
+#include "S10_Enumerating_available_physical_devices.h"
+#include "S11_Checking_available_device_extensions.h"
+#include "S12_Getting_features_and_properties_of_a_physical_device.h"
+#include "S13_Checking_available_queue_families_and_their_properties.h"
+#include "S14_Selecting_index_of_a_queue_family_with_desired_capabilities.h"
+#include "S15_Creating_a_logical_device.h"
+#include "S16_Loading_device_level_functions.h"
+#include "S17_Getting_a_device_queue.h"
+#include "S18_Creating_a_logical_device_with_geometry_shaders_and_graphics_queue.h"
+#include "S19_Destroying_a_logical_device.h"
+#include "S20_Destroying_a_Vulkan_Instance.h"
+#include "S21_Releasing_a_Vulkan_Loader_library.h"
+
+#endif //VKCOOKBOOK_CH01_INSTANCE_AND_DEVICES_H
