ddfc56a2268414bc0227983116d86a52e7e08abf
yanjun <yangyj19931231@gmail.com>
2017-06-19 22:31:05 +0800

    CH02 Image Presentation

---
+++ book/CH02/CH02_Image_Presentation.h
@@ -0,0 +1,27 @@
+//
+// Created by aicdg on 2017/6/19.
+//
+
+#ifndef VKCOOKBOOK_CH02_IMAGE_PRESENTATION_H
+#define VKCOOKBOOK_CH02_IMAGE_PRESENTATION_H
+
+#include "S01_Creating_a_Vulkan_Instance_with_WSI_extensions_enabled.h"
+#include "S02_Creating_a_presentation_surface.h"
+#include "S03_Selecting_a_queue_family_that_supports_presentation_to_a_given_surface.h"
+#include "S04_Creating_a_logical_device_with_WSI_extensions_enabled.h"
+#include "S05_Selecting_a_desired_presentation_mode.h"
+#include "S06_Getting_capabilities_of_a_presentation_surface.h"
+#include "S07_Selecting_a_number_of_swapchain_images.h"
+#include "S08_Choosing_a_size_of_swapchain_images.h"
+#include "S09_Selecting_desired_usage_scenarios_of_swapchain_images.h"
+#include "S10_Selecting_a_transformation_of_swapchain_images.h"
+#include "S11_Selecting_a_format_of_swapchain_images.h"
+#include "S12_Creating_a_swapchain.h"
+#include "S13_Getting_handles_of_swapchain_images.h"
+#include "S14_Creating_a_swapchain_with_R8G8B8A8_format_and_a_MAILBOX_present_mode.h"
+#include "S15_Acquiring_a_swapchain_image.h"
+#include "S16_Presenting_an_image.h"
+#include "S17_Destroying_a_swapchain.h"
+#include "S18_Destroying_a_presentation_surface.h"
+
+#endif //VKCOOKBOOK_CH02_IMAGE_PRESENTATION_H
