630d4dc91a79bf822cebe95368a6a937b62eb84b
攻伤菊菊长 <yangyj19931231@gmail.com>
2017-06-24 23:24:47 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -36,7 +36,7 @@ cd build
 cmake.exe .. -DVK_USE_PLATFORM=WIN32 -G "Visual Studio 14 2015 Win64"
 
 if exist VulkanCookbook.sln (
-	start "" "VulkanCookbook.sln"
+	start "" "vkCookBook.sln"
 ) else (
 	echo "Error occurred during solution creation!"
 )
